ddbf348da3fcf0023c08090fb05a27b42b72cd67
Peter Fogg <peter.p.fogg@gmail.com>
2023-01-10 19:31:51 -0700

    JS slider usability improvements

--- html/js/autoslider.js
+++ html/js/autoslider.js
@@ -4,17 +4,25 @@ window.onload = function () {
     // Show the buttons if JS is enabled
     document.getElementById('slider-buttons').classList.remove('hidden');
 
-    document.getElementById('slider-back').onclick = function() {
+    document.getElementById('slider-back').onclick = function(event) {
+        event.target.disabled = true;
         let div = document.getElementById('slides');
         let position = div.scrollLeft;
         let newPosition = Math.max(position - slideWidth);
-        div.scrollTo(newPosition, 0);
+        div.scrollTo({left: newPosition, behavior: 'smooth'});
+        window.setTimeout(() => {
+            event.target.disabled = false;
+        }, 500);
     };
 
-    document.getElementById('slider-forward').onclick = function() {
+    document.getElementById('slider-forward').onclick = function(event) {
+        event.target.disabled = true;
         let div = document.getElementById('slides');
         let position = div.scrollLeft;
         let newPosition = Math.min(position + slideWidth);
-        div.scrollTo(newPosition, 0);
+        div.scrollTo({left: newPosition, behavior: 'smooth'});
+        window.setTimeout(() => {
+            event.target.disabled = false;
+        }, 500);
     };
 };
