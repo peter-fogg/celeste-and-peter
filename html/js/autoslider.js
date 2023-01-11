f6182cb36f3c5ff7adb61f08af4d3bf3a107f785
Peter Fogg <peter.p.fogg@gmail.com>
2023-01-10 18:23:04 -0700

    slider buttons when JS enabled

---
+++ html/js/autoslider.js
@@ -0,0 +1,22 @@
+// TODO this is not especially DRY
+const numSlides = 13 - 1;
+const slideWidth = window.matchMedia('(max-width: 600px)').matches ? 300 : 500;
+
+window.onload = function () {
+    // Show the buttons if JS is enabled
+    document.getElementById('slider-buttons').classList.remove('hidden');
+
+    document.getElementById('slider-back').onclick = function() {
+        let div = document.getElementById('slides');
+        let position = div.scrollLeft;
+        let newPosition = Math.max(position - slideWidth);
+        div.scrollTo(newPosition, 0);
+    };
+
+    document.getElementById('slider-forward').onclick = function() {
+        let div = document.getElementById('slides');
+        let position = div.scrollLeft;
+        let newPosition = Math.min(position + slideWidth);
+        div.scrollTo(newPosition, 0);
+    };
+};
