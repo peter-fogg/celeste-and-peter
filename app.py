6be99ddd6e7bfcbb3e913c5b1f502b25966322c4
Peter Fogg <peter.p.fogg@gmail.com>
2023-03-20 19:35:11 -0600

    converted basics to flask app

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ import flask as f
 app = f.Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'hello'
+def index():
+    return f.render_template('index.html')
+
+@app.route('/details')
+def details():
+    return f.render_template('details.html')
+
+@app.route('/durango')
+def durango():
+    return f.render_template('durango.html')
