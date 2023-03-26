12f0cbfe242b36cb12bcd872f5554f6bde3b7548
Peter Fogg <peter.p.fogg@gmail.com>
2023-03-26 10:53:49 -0600

    basic RSVP form

--- app.py
+++ app.py
@@ -14,6 +14,18 @@ def details():
 def durango():
     return f.render_template('durango.html')
 
+@app.route('/rsvp', methods=['GET'])
+def rsvp():
+    return f.render_template('rsvp.html')
+
+@app.route('/rsvp', methods=['POST'])
+def rsvp_post():
+    form = f.request.form
+    if form.get('coming') is None:
+        return f.render_template('not_coming.html')
+    # TODO: add error handling in case of missing data/wrong name/etc.
+    return f.render_template('thanks.html', info=dict(form))
+
 @app.errorhandler(404)
 def not_found(error):
     return f.render_template('404.html')
